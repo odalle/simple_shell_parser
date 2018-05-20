0bf5c088db0a88d913cbb65b81c36b0df6f24f2f
Olivier Dalle <olivier.dalle@unice.fr>
2018-05-20 14:36:38 +0200

    Updated example to test iterability and typoe tester methods.

--- example.py
+++ example.py
@@ -2,4 +2,10 @@ import lexer as ssp
 
 result = ssp.get_parser().parse('foo toto > bar 2>> baz | fizz >> fuzzz << EOF | goo > gooo < gaa 2> ga | dummy')
 
-for p in result: print(p)
+for p in result: 
+    print(p)
+    for redir in p.getRedirs():
+        print('\t', redir, sep='', end='')
+        if redir.isOutput(): print(' is:OUTPUT')
+        if redir.isInput(): print(' is:INPUT')
+        if redir.isError(): print(' is:ERROR')
